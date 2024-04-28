65ba1b95fec6d57ae80c2e00bd7c59a7a3e3aa44
erdemduran <111234949+erdemduran@users.noreply.github.com>
2024-04-28 20:45:25 +0300

    Ders.cs

---
+++ Ders.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Model_Klasörü.Models
+{
+    public class Ders
+    {
+        public string Ad { get; set; }
+        public string Seviye { get; set; }
+        public string Öğretmen { get; set; }
+    }
+
+}
